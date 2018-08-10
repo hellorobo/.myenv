dee41fbdd2855032be101eaec560239b1c4bc81b
Robert J <robert.jakowenko@nordcloud.com>
2018-08-10 14:35:14 +0200

    initial commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd ~
+ln -s ~/.myenv/vim/vimrc .vimrc
+ln -s ~/.myenv/bash/bashrc .bashrc
+ln -s ~/.myenv/bash/bash_profile .bash_profile
+ln -s ~/.myenv/bash/bash_aliases .bash_aliases
+cd -
